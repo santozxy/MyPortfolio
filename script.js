cc2520d9229c04c151ad595373e487c9ec91b945
Monnuery Júnior <118072648+santozxy@users.noreply.github.com>
2023-08-05 03:44:28 -0300

    Adicionando arquivos e criando css

---
+++ script.js
@@ -0,0 +1,15 @@
+function typingLetter() {
+    function active(element) {
+      const arrLetter = element.innerHTML.split("");
+      element.innerHTML = "";
+      arrLetter.forEach((letter, i) => {
+        setTimeout(() => {
+          element.innerHTML += letter;
+        }, 75 * i);
+      });
+    }
+    const title = document.querySelector(".typing");
+    active(title);
+  }
+  typingLetter();
+  
