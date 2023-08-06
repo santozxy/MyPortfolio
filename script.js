f022d7c1a7a1215852e633c4cd4fa724c4d57863
Monnuery Júnior <118072648+santozxy@users.noreply.github.com>
2023-08-06 09:19:16 -0300

    Alterações em seção About

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+function openTab(evt, nav) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(nav).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("experience").style.display = "block";
+});
+
 function typingLetter() {
   function active(element) {
     const arrLetter = element.innerHTML.split("");
