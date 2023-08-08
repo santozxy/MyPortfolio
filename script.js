d42fac455abc299c5dade77023d180aa58d4d8e4
Monnuery Júnior <118072648+santozxy@users.noreply.github.com>
2023-08-08 15:23:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-function openTab(evt, nav) { // Navegação Tab Bar 
-  var i, tabcontent, tablinks;
+// Navegação Tab Bar
+function openTab(evt, nav) {
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  const tablinks = document.getElementsByClassName("tablinks");
 
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
+  for (let i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
@@ -19,7 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("experience").style.display = "block";
 });
 
-function typingLetter() { // Efeito de piscar as letras
+// Efeito de piscar as letras
+function typingLetter() {
   function active(element) {
     const arrLetter = element.innerHTML.split("");
     element.innerHTML = "";
@@ -29,37 +27,38 @@ function typingLetter() { // Efeito de piscar as letras
       }, 75 * i);
     });
   }
+
   const title = document.querySelector(".typing");
   active(title);
 }
 typingLetter();
 
+// Função para realizar o download do PDF
 function downloadPDF() {
-  //Função para realizar o download do pdf
-  var pdfUrl = "assets/cv.pdf";
-  var link = document.createElement("a");
+  const pdfUrl = "assets/cv.pdf";
+  const link = document.createElement("a");
   link.href = pdfUrl;
   link.setAttribute("download", "Currículo de Monnuery Júnior.pdf");
   link.click();
 }
 document.getElementById("btn-cv").addEventListener("click", downloadPDF);
 
+// Contadores de curtidas
 document.addEventListener("DOMContentLoaded", function () {
   const likeButtons = document.querySelectorAll(".button-icon");
   const likeCountElements = document.querySelectorAll("#count-like");
 
-  // Função para inicializar os contadores de curtidas se não estiverem definidos no localStorage
   function initializeLikes() {
     for (let i = 0; i < likeCountElements.length; i++) {
       const cardId = i + 1;
-      if (!localStorage.getItem(`card${cardId}Likes`)) {
+      const storedLikes = localStorage.getItem(`card${cardId}Likes`);
+      if (storedLikes === null) {
         localStorage.setItem(`card${cardId}Likes`, "0");
       }
       updateLikeCountDisplay(cardId);
     }
   }
 
-  // Função para incrementar o contador de curtidas em um card
   function incrementLikes(cardId) {
     const currentLikes = parseInt(localStorage.getItem(`card${cardId}Likes`));
     const newLikes = currentLikes + 1;
@@ -67,17 +66,14 @@ document.addEventListener("DOMContentLoaded", function () {
     updateLikeCountDisplay(cardId);
   }
 
-  // Função para atualizar a exibição do contador de curtidas em um card
   function updateLikeCountDisplay(cardId) {
     const likes = localStorage.getItem(`card${cardId}Likes`);
     likeCountElements[cardId - 1].textContent = likes;
   }
 
-  // Configurar os botões de curtir
   likeButtons.forEach((button, index) => {
     button.addEventListener("click", () => incrementLikes(index + 1));
   });
 
-  // Inicializar os contadores de curtidas quando a página carregar
   initializeLikes();
 });
